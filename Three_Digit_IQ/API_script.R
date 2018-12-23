1877888ffe3f79704cc02433d5a68c83bb79cd89
Brian Maxwell <bmaxwell99@gmail.com>
2018-12-23 09:33:40 -0800

    events endpoint doesnt appear to be it?

--- Three_Digit_IQ/API_script.R
+++ Three_Digit_IQ/API_script.R
@@ -4,9 +4,26 @@ library(magrittr)
 library(dplyr)
 
 
-key<- readLines('C:/Users/dark_/Desktop/logs_pub_key.txt')
-base_uri <- 'https://www.warcraftlogs.com/v1'
-endpoint <-paste0('/report/events/', raid_data_id)
+if(Sys.info()["user"] == "dark_") {
+  PROJECTDIR <- file.path("C:", "Users", "dark_","Desktop")
+  key_name <- "/logs_pub_key.txt"
+  
+} else if (Sys.info()["user"] == 'desktop' ){
+  PROJECTDIR <- file.path()
+  fileName <- "josh_results.csv"
+
+} 
+
+setwd(PROJECTDIR)
+
+#Identifying info for the logs you want to pull
+raid_data_id <- "8QjYhJFrZtgVb2mA"
+
+
+key<- paste0(getwd(), key_name) %>% 
+  readLines()
+base_uri <- 'https://www.warcraftlogs.com:443/v1'
+endpoint <-paste0('/report/fights/', raid_data_id)
 
 res <- GET(paste0(base_uri, endpoint, '?api_key=', key))
 http_status(res)
@@ -14,10 +31,3 @@ content <- content(res,  'text' )
 event_data <- fromJSON(content)
 View(event_data)
 
-raid_data <- 
-  data %>% 
-  filter(zone == 19)
-
-raid_data %>% View()
-
-raid_data_id <- "v2Q8DzGVKxrJanc4"

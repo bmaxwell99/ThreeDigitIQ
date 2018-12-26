684c006b2106162a8d8859a561749cac8c6f675c
Brian Maxwell <bmaxwell99@gmail.com>
2018-12-25 18:24:26 -0800

    fight api needs chaining to table api

--- API_play/API_script.R
+++ API_play/API_script.R
@@ -13,23 +13,50 @@ if(Sys.info()["user"] == "dark_") {
   key_name <- "/logs_pub_key.txt"
 } 
 
-setwd(PROJECTDIR)
-
-#Identifying info for the logs you want to pull
-raid_data_id <- "8QjYhJFrZtgVb2mA"
-start_time = '17'
-end_time = '17'
-
+setwd(PROJECTDIR) 
 key<- paste0(getwd(), key_name) %>% 
   readLines()
-
 base_uri <- 'https://www.warcraftlogs.com:443/v1'
-endpoint <-paste0('/report/events/', raid_data_id, '?start=', start_time, '&end=', end_time)
-res <- GET(paste0(base_uri, endpoint, '&api_key=', key))
-http_status(res)
-content <- content(res,  'text' )
-event_data <- fromJSON(content)
-View(event_data)
 
-data.frame(event_data)
-View(event_data)
+
+#core function for running the api and converting the json
+run_api <- function(endpoint) {
+  res <- GET(paste0(base_uri, endpoint, 'api_key=', key))
+  http_status(res)
+  content <- content(res,  'text' )
+  x <- fromJSON(content)
+}
+
+
+#functions creating the endpoints
+events_endpoint <- function(raid_id, start_time, end_time) {
+  x <- paste0('/report/events/', raid_data_id, '?', 'start=', start_time, '&end=', end_time, '&')
+}
+
+fights_endpoint <- function(raid_id) {
+  x <- paste0('/report/fights/' , raid_id, '?')
+}
+
+tables_endpoint <- function(view, raid_id, start_time, end_time, ability_id){
+  x <- paste0('/report/tables/', view, raid_id, '?', 'start=', start_time, '&end=', end_time, '&')
+  
+}
+
+#The events data
+events_endpoint("htxJHQwTK3LapV2Z", '17', '17') %>% 
+  run_api()
+
+
+#The fights endpoint data
+fights_data <- 
+  fights_endpoint('htxJHQwTK3LapV2Z') %>% 
+  run_api
+
+#Purifying flame data
+boss_fight_data <- 
+  fights_data[['fights']] %>% 
+  filter(boss == '2141') 
+
+
+
+
